0a751a3788dc739a9bbd9ea1d33c911597ad3026
Popovici Ioana <121975704+ipsupp@users.noreply.github.com>
2024-06-28 17:35:38 +0300

    Add files via upload

---
+++ Crossplatform/c_sharp_version/mpp_winforms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+
+namespace mpp_winforms
+{
+    class Program
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        [STAThread]
+
+
+        static void Main()
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+
+        }
+    }
+}
+
---
+++ Crossplatform/c_sharp_version/mpp_winforms/state/CurrentAppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpp_winforms
+{
+    public static class CurrentAppData
+    {
+        public static string connectionString;
+    }
+}
